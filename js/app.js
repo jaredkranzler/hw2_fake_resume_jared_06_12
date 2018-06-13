e703d1a9bff47a73977fba4a57aed8f7c2a39e1a
Jared Kranzler <kranzler312@gmail.com>
2018-06-13 08:32:22 -0500

    donno where my git is saving if at all

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
 console.log("javascript work!");
+
+/*
+for (i = 0; i <= 99; i++){
+console.log(i);
+}*/
+
+/*for (i = 99; i >= 0; i--){
+console.log(i);
+}*/
+
+/*for (i = 0; i <= 99; i += 2){
+console.log(i);
+}*/
+
+/*for (i = 98; i >= 0; i -= 2){
+console.log(i);
+}*/
+/*
+for (i = 49; i <= 72; i++){
+console.log(i);
+}*/
+
+/*for (i = 81; i >= 26; i--){
+console.log(i);
+}*/
+
+
+/*for (i = 3; i <= 90; i += 3){
+console.log(i);
+}*/
+
+
+
+
